0760540326fc4690a43d474cc81d9a2a9a54e8da
jbatphoto <justin.battenfield@lifechurch.tv>
2015-01-27 14:39:53 -0600

    Updated Ruby version

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
-ruby "2.1.1"
-
+ruby '2.1.2'
 gem 'rails', '4.2.0'
 gem 'pg'
 gem 'carrierwave'
--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.serve_static_assets = true
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 
 module PortfolioSite
   class Application < Rails::Application
+    config.serve_static_files = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
